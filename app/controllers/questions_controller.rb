791fd84f65f833308a4f6a77f7e854243d977a68
AlinaKhalilova <akhalilova@alumni.escaet.fr>
2018-02-12 14:46:27 +0100

    exo1 : ask

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,7 +8,7 @@ class QuestionsController < ApplicationController
     #recuperer reponse utilisateurs de l'url
     if @question == "I am going to work"
       @answer = "Great"
-     elsif @question.include?("%3F")
+     elsif @question.include?("?")
     @answer = "Silly question, get dressed and go to work!"
     else
     @answer = "I don't care, get dressed and go to work!"
